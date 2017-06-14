fbc71b156c497bbb68016642e91b62ba4a9c8787
SarahVillareal <sarahvilla2003@yahoo.com>
2017-06-14 15:58:37 -0500

    Added Search Function/Bar

--- src/main/java/com/codeup/adlister/dao/ListAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/ListAdsDao.java
@@ -44,6 +44,11 @@ public class ListAdsDao implements Ads {
         return null;
     }
 
+    @Override
+    public List<Ad> getAdsByTerm(String search) {
+        return null;
+    }
+
     private List<Ad> generateAds() {
         List<Ad> ads = new ArrayList<>();
         ads.add(new Ad(
